d4fafd5c3cb1daad61722a596163f2089ee6bc59
kevin@tonkworks.com <kevin@tonkworks.com>
2016-10-16 12:08:04 -0400

    Added more image checks

--- src/train.sh
+++ src/train.sh
@@ -1,7 +1,11 @@
 python example/retrain.py \
 --bottleneck_dir=/tf_files/bottlenecks \
---how_many_training_steps 4000 \
+--how_many_training_steps 6000 \
 --model_dir=/tf_files/inception \
 --output_graph=/tf_files/retrained_graph.pb \
 --output_labels=/tf_files/retrained_labels.txt \
---image_dir /tf_files/data
+--image_dir /tf_files/data #\
+#--random_crop=5 \
+#--random_scale=5 \
+#--random_brightness=5 \ 
+#--flip_left_right=1
